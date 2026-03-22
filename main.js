70fd11aabc17e841a3aa662cf0ea955d9d9f5c97
julia <julyeoli12@gmail.com>
2026-03-22 10:18:30 -0300

    fisrt commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const projetos = [
+  {
+    nome: "Projeto 1",
+    descricao: "Descrição do projeto 10000",
+    link: "#"
+  },
+  {
+    nome: "Projeto 2",
+    descricao: "Descrição do projeto 2",
+    link: "#"
+  },
+  {
+    nome: "Projeto 3",
+    descricao: "Descrição do projeto 3",
+    link: "#"
+  }
+];
+
+const container = document.getElementById("lista_projetos");
+
+projetos.forEach(projeto => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <h3>${projeto.nome}</h3>
+    <p>${projeto.descricao}</p>
+    <a href="${projeto.link}">Ver projeto</a>
+  `;
+
+  container.appendChild(card);
+});
